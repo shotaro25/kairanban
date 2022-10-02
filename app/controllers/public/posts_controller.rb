4ed0777f596ead2d9f7fd72a697bc31ce3521b00
EC2 Default User <ec2-user@ip-172-31-19-245.ap-northeast-1.compute.internal>
2022-10-02 07:37:22 +0000

    検索機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,9 +2,14 @@ class Public::PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
-    @posts_true = Post.where(is_active: true).order('id DESC').page(params[:page])
-    @posts_true_count = Post.where(is_active: true).count
-    @posts = Post.where('name LIKE ?', "%#{params[:search]}%") if params[:search].present?
+   # @posts_true = Post.where(is_active: true).order('id DESC').page(params[:page])
+   # @posts_true_count = Post.where(is_active: true).count
+    if params[:search].present?
+     
+      @posts = Post.where('name LIKE ?', "%#{params[:search]}%").where(is_active: true).page(params[:page])
+    else
+       @posts = Post.where(is_active: true).page(params[:page])
+    end
   end
 
   def show
